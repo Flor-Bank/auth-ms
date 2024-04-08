8761c79e9b678ecfd59934092bb7e4b43ce80b6b
florMignini <flormignini29@gmail.com>
2024-04-08 08:06:25 -0300

    controller settings

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @MessagePattern('auth.login.user')
+  registerUser() {
+    return `register user endpoint`;
+  }
 }
