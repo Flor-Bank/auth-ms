a712c7f6ac92b8ea3f7da89dee22daf3281892f7
florMignini <flormignini29@gmail.com>
2024-05-26 19:40:55 -0300

    validation pipe update

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
-import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -19,9 +18,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
-      exceptionFactory: (validationErrors: ValidationError[] = []) => {
-        return new BadRequestException(validationErrors);
-      },
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
